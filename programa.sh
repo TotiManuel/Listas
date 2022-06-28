d752e7e2b49ecaea2cbecce0d71461ab01c65d60
TotiManuel <manuel.mandaio@gmail.com>
2022-06-28 15:31:07 -0300

    añadir archivos en Listas

---
+++ programa.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/maldevel/IPGeoLocation
+git clone https://github.com/DeepSociety/wiki-termux
+git clone https://github.com/htr-tech/zphisher
+git clone https://github.com/Hax4us/TermuxBlack
+git clone https://github.com/sherlock-project/sherlock
+git clone https://github.com/Ghost-crypto-exe/Virus-tools
+git clone https://github.com/orkestral/venom
